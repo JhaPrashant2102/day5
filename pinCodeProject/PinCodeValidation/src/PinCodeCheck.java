26de30f0f66f0d17f62bfe8987ded037949d4ad2
JhaPrashant2102 <jhaprashant2102@gmail.com>
2020-09-28 17:15:31 +0530

    UC2 added

--- pinCodeProject/PinCodeValidation/src/PinCodeCheck.java
+++ pinCodeProject/PinCodeValidation/src/PinCodeCheck.java
@@ -13,6 +13,11 @@ public class PinCodeCheck {
 		//System.out.println("Enter the String :");
 		//Scanner sc = new Scanner(System.in);
 		//String inputString =  sc.nextLine();
+		
+		//UC1
 		System.out.println(Pattern.matches("[0-9]{6}","400088"));	
+		
+		//UC2
+		System.out.println(Pattern.matches("[0-9]{6}","A400088"));
 	}
 }
